e7d98463587fad9cf45ba566645453c283234a78
kwabenahcode <mrofori507@gmail.com>
2024-03-06 03:51:40 +0000

    creating forgot password screen

--- frontend/src/screens/ForgotPasswordScreens/FogotPasswordScreen.jsx
+++ frontend/src/screens/ForgotPasswordScreens/FogotPasswordScreen.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import {Row, Col, Container} from 'react-bootstrap'
+import React, { useState } from 'react'
+import {Row, Col, Container, Form} from 'react-bootstrap'
 import { BiSolidShoppingBags } from "react-icons/bi";
 
 
 function FogotPasswordScreen() {
+  const [email, setEmail] = useState()
   return (
     <div>
       <Container className='main-container'>
@@ -11,6 +12,26 @@ function FogotPasswordScreen() {
           <div className='logo'>
             <BiSolidShoppingBags color='#00A6A6' className='logo-icon'/> 
             <span className='logo-text'>Studentpreneurs HUB</span>
+          </div>
+
+          <div className='forgotpassword-text'>
+            <span className='forgotpassword-question'>Forgot Password</span>
+            <Row className='instruction-row'>
+              <span className='reset-instructions'>No worries, we'll send you reset instructions.</span>
+            </Row>
+          </div>
+
+          <div className='forgotpassword-textbox'>
+            <label>E-mail</label>
+            <Form className='form'>
+              <input 
+                type='email'
+                placeholder='name@example.com'
+                value={email}
+                // onChange={setEmail}
+                
+              />
+            </Form>
           </div>
 
         </Row>
