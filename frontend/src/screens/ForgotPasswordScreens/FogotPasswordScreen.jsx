e33f1c0869f04143f4718a020c0de38b1f272374
kwabenahcode <mrofori507@gmail.com>
2024-03-06 00:37:40 +0000

    creating forgot password screen

--- frontend/src/screens/ForgotPasswordScreens/FogotPasswordScreen.jsx
+++ frontend/src/screens/ForgotPasswordScreens/FogotPasswordScreen.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Row, Col, Container} from 'react-bootstrap'
+import { BiSolidShoppingBags } from "react-icons/bi";
+
 
 function FogotPasswordScreen() {
   return (
-    <div>FogotPasswordScreen</div>
+    <div>
+      <Container className='main-container'>
+        <Row className='firstRow'>
+          <div className='logo'>
+            <BiSolidShoppingBags color='#00A6A6' className='logo-icon'/> 
+            <span>Studentpreneurs HUB</span>
+          </div>
+
+        </Row>
+      </Container>
+      
+    </div>
   )
 }
 
