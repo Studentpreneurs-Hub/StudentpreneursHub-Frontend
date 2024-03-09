9d8ceab08a6d8776f0b33ffdae1facd339174d64
RonenTGreat <ronenh53@gmail.com>
2024-03-09 20:14:06 +0000

    Done with OTP Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Routes, Route, Router} from 'react-router-dom'
 import FogotPasswordScreen from './screens/ForgotPasswordScreens/FogotPasswordScreen';
 import SetNewPasswordScreen from './screens/ForgotPasswordScreens/SetNewPasswordScreen';
 import SignUp from './screens/SignUpOTPScreens/SignUp';
+import OTPScreen from './screens/SignUpOTPScreens/OTPScreen';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Container>
           {/* <FogotPasswordScreen /> */}
           {/* <SetNewPasswordScreen /> */}
-          <SignUp />
+          {/* <SignUp /> */}
+          <OTPScreen />
         </Container>
       </main>
     </div>
---
+++ frontend/src/screens/SignUpOTPScreens/OTPScreen.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { Row, Button, Container, Form } from "react-bootstrap";
+import logo from "../../assets/logo.png";
+
+const OTPScreen = () => {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [number3, setNumber3] = useState("");
+  const [number4, setNumber4] = useState("");
+  const [number5, setNumber5] = useState("");
+  const [number6, setNumber6] = useState("");
+
+  const handleSubmit = (e) => {
+    // Validation code
+
+    if(number1 === "" || number2 === "" || number3 === "" || number4 === "" || number5 === "" || number6 === ""){
+        alert("Field(s) can't be blank")
+    }else{
+        alert(`${number1} ${number2} ${number3} ${number4} ${number5} ${number6}`)
+    }
+  };
+
+  return (
+    <>
+      <Container className="Container Verify">
+        <Row>
+          <img src={logo} alt="logo" className="logo" />
+
+          <div className="verify">
+            <div className="verify-heading">Check your email</div>
+            <div className="verify-sub">
+              Enter the 6 digit code sent to <span>nathan@gmail.com</span> to
+              verify your account.
+            </div>
+          </div>
+
+          <div className="verify-form">
+            <Form className="boxes">
+              <input
+                type="text"
+                id="number1"
+                value={number1}
+                maxLength={1}
+                onChange={(e) => setNumber1(e.target.value)}
+              />
+              <input
+                type="text"
+                id="number2"
+                value={number2}
+                maxLength={1}
+                onChange={(e) => setNumber2(e.target.value)}
+              />
+              <input
+                type="text"
+                id="number3"
+                value={number3}
+                maxLength={1}
+                onChange={(e) => setNumber3(e.target.value)}
+              />
+              <input
+                type="text"
+                id="number4"
+                value={number4}
+                maxLength={1}
+                onChange={(e) => setNumber4(e.target.value)}
+              />
+              <input
+                type="text"
+                id="number5"
+                value={number5}
+                maxLength={1}
+                onChange={(e) => setNumber5(e.target.value)}
+              />
+              <input
+                type="text"
+                id="number6"
+                value={number6}
+                maxLength={1}
+                onChange={(e) => setNumber6(e.target.value)}
+              />
+            </Form>
+          </div>
+          <Button className="btn verify-btn" onClick={handleSubmit}>
+            Verify
+          </Button>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default OTPScreen;
