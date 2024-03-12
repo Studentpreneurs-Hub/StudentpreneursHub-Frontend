cac995289227fc368a826cf5d9ab7aed8d2a38cd
kwabenahcode <mrofori507@gmail.com>
2024-03-12 23:51:23 +0000

    creating the Header...

--- frontend/src/components/Navbar/Header.jsx
+++ frontend/src/components/Navbar/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown, Button } from 'react-bootstrap'
 import './header.css'
+import logo from "../../assets/logo.png";
 
 
 function Header() {
@@ -8,17 +9,29 @@ function Header() {
     <header>
       <Navbar expand="lg" className="navbar">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img src={logo} className="signup__logo" alt="logo" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="navbar-nav m-auto">
-              <Nav.Link href="#home" className='nav_text'>About Us</Nav.Link>
-              <Nav.Link href="#link" className='nav_text'>FAQs</Nav.Link>
-              <NavDropdown title="Categories" id="basic-nav-dropdown" className='dropdown-link'>
-                <NavDropdown.Item href="#action/3.1" >Action</NavDropdown.Item>
+            <Nav className="navbar-nav mx-auto">
+              <Nav.Link href="#about-us" className="nav-text">
+                About Us
+              </Nav.Link>
+              <Nav.Link href="#FAQs" className="nav-text">
+                FAQs
+              </Nav.Link>
+              <NavDropdown title="Categories" id="basic-nav-dropdown" className="dropdown-link">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               </NavDropdown>
+            </Nav>
+            <Nav className="header-second ms-auto">
+              <Nav.Link href="#login" className="nav-text">
+                Login
+              </Nav.Link>
+              <Button className="btn btn-primary">Start selling</Button>
             </Nav>
           </Navbar.Collapse>
         </Container>
