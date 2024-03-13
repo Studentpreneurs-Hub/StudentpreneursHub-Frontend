f51b6f6367341a0c24b5264ccaef5b755cd0aa36
kwabenahcode <mrofori507@gmail.com>
2024-03-13 03:59:54 +0000

    creating landing page

--- frontend/src/screens/LandingPage/Landing.jsx
+++ frontend/src/screens/LandingPage/Landing.jsx
@@ -8,6 +8,18 @@ function Landing() {
         <div className='langingImage-container'>
             <img src={landingImage} alt="uu" className='langingImage' />
         </div>
+
+        <div className='sell_Faster_main_div'>
+            <div className='sell_faster_div'>
+                <span className='sell_faster_text'>Sell faster today</span>
+                <button>Start Selling</button>
+            </div>
+
+            <div>
+                ju
+            </div>
+
+        </div>
     </div>
   )
 }
