9eb4e8a802d026f4723163db23cd55c9fa641343
kwabenahcode <mrofori507@gmail.com>
2024-03-13 05:22:45 +0000

    creating landing page

--- frontend/src/screens/LandingPage/Landing.jsx
+++ frontend/src/screens/LandingPage/Landing.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import landingImage from '../../assets/landingImage.jpeg'
 import './landing.css'
+import { Button } from 'react-bootstrap'
+import arrowRight from '../../assets/arrowRight.jpeg'
 
 function Landing() {
   return (
@@ -11,12 +13,18 @@ function Landing() {
 
         <div className='sell_Faster_main_div'>
             <div className='sell_faster_div'>
-                <span className='sell_faster_text'>Sell faster today</span>
-                <button>Start Selling</button>
+                <span className='sell_faster_text'>Sell faster today.</span>
+                <br />
+                <Button className='start_selling'>Start Selling</Button>
             </div>
 
-            <div>
-                ju
+            <div className='arrow_div'>
+                <img src={arrowRight} alt="" />
+                <p>
+                Join our vibrant hub of student entrepreneurs! 
+                Discover unique products and innovative services right on campus, 
+                from gourmet delights to the latest in tech and fashion.
+                </p>
             </div>
 
         </div>
