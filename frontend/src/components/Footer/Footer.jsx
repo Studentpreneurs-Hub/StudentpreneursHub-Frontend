4fb2d5888f02dbdb1865b9338a321cbf19ccec52
kwabenahcode <mrofori507@gmail.com>
2024-03-26 17:03:36 +0000

    creating the footer Component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import "./footer.css";
 import { BiSolidShoppingBags } from "react-icons/bi";
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 function Footer() {
   return (
     <div>
       <footer>
-        <div className="logo">
-          <BiSolidShoppingBags color="#00A6A6" className="logo-icon" />
-          <span className="logo-text">Studentpreneurs HUB.</span>
+        <div className="footer_sub_div">
+          <div className="logo">
+            <BiSolidShoppingBags color="#00A6A6" className="logo-icon" />
+            <span className="logo-text">Studentpreneurs HUB.</span>
+          </div>
+
+          <div className="footer_icons">
+            <FaTwitter color="#ffffff" size={20} />
+            <FaFacebookF color="#ffffff" size={20} />
+            <FaInstagram color="#ffffff" size={20} />
+            <FaLinkedin color="#ffffff" size={20} />
+          </div>
         </div>
       </footer>
     </div>
