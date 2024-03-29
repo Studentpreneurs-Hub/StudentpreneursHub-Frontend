ac3d24f2bde4bfb3714926c7016cd911312ac056
kwabenahcode <mrofori507@gmail.com>
2024-03-29 00:37:35 +0000

    creating product details page

--- frontend/src/screens/OnClickProductScreen/OnClickProduct.jsx
+++ frontend/src/screens/OnClickProductScreen/OnClickProduct.jsx
@@ -6,7 +6,7 @@ import side from "../../assets/iphone12-sd.jpg";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 import Header from "../../components/Navbar/Header";
 import Footer from "../../components/Footer/Footer";
-import sheldon from '../../assets/sheldon.jpg'
+import sheldon from "../../assets/sheldon.jpg";
 import { FaTwitter } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -32,8 +32,8 @@ function OnClickProduct() {
     <div>
       <Header />
       <div className="product_details_main_div">
-        <div style={{ marginLeft: "6rem" }}>
-          <h3>Iphone 12 pro max</h3>
+        <div>
+          <h3 className="product_name">Iphone 12 pro max</h3>
           <div className="product_img">
             <img
               src={images[currentImage]}
@@ -65,18 +65,21 @@ function OnClickProduct() {
         </div>
         <div className="personal_card">
           <div className="contact_details_card">
-                <img src={sheldon} alt="" style={{clipPath:'circle()', width:'57px'}}/>
-                <div>
-                    <h5>Maxine Apple Store</h5>
-                    <span>Gideon Maxi Appiah</span>
-                </div>
+            <img
+              src={sheldon}
+              alt=""
+              style={{ clipPath: "circle()", width: "57px" }}
+            />
+            <div>
+              <h5 className="store_name">Maxine Apple Store</h5>
+              <span className="contact_name">Gideon Maxi Appiah</span>
+            </div>
           </div>
           <div className="social_icons">
-          <FaTwitter  size={20} />
-            <FaFacebookF  size={20} />
-            <FaInstagram  size={20} />
-            <FaLinkedin  size={20} />
-
+            <FaTwitter size={20} />
+            <FaFacebookF size={20} />
+            <FaInstagram size={20} />
+            <FaLinkedin size={20} />
           </div>
         </div>
       </div>
