d0afee66dfee2bf730f8f05e1fdfd7dfa77f64e8
kwabenahcode <mrofori507@gmail.com>
2024-03-30 17:11:51 +0000

    creating Login Screen

---
+++ frontend/src/screens/LoginScreen/Login.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import { Button, Container, Form } from "react-bootstrap";
+import logo from "../../assets/logo.png";
+import show from "../../assets/Show.png";
+import { Link } from "react-router-dom";
+
+function Login() {
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+    const[showPassword, setShowPassword] = useState(false)
+
+    const handleTogglePassword = () =>{
+        setShowPassword(!showPassword)
+    }
+
+
+  return (
+    <div>
+        <Container className="signup">
+        <div>
+          <img src={logo} className="signup__logo" alt="logo" />
+
+          <div className="signup__header">
+            <div className="signup__header__title">Login</div>
+            <div className="signup__header__subtitle">
+                Welcome back! Please enter your details
+            </div>
+          </div>
+
+          <div className="signup__form">
+
+            <Form>
+              <label className="signup__form__label">Email</label>
+              <input
+                type="email"
+                className="signup__input"
+                id="email"
+                placeholder="name@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form>
+
+            <Form>
+              <label className="signup__form__label">Password</label>
+              <div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className="signup__input"
+                  id="password"
+                  placeholder="Your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <img
+                  src={show}
+                  className="showIcon"
+                  onClick={handleTogglePassword}
+                  alt="show/hide password img"
+                />
+              </div>
+            </Form>
+            
+              <Button className="signup__btn" onClick={handleSubmit}>
+                Create Account
+              </Button>
+          </div>
+
+          <div className="signup__info">
+            <p className="signup__info__terms">
+              By signing up, you agree to our{" "}
+              <a href="/#">Terms of Service & Privacy Policy</a>
+            </p>
+            <p className="signup__info__login">
+              Already have an account?{" "}
+              <Link to="/Login" style={{ color: "#00A6A6" }}>
+                Log in
+              </Link>
+            </p>
+          </div>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default Login
