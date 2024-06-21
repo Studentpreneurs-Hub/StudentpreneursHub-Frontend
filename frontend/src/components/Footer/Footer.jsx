9c7a3cefb3358e2117c95e54d1971cee09072b4d
RonenTGreat <ronenh53@gmail.com>
2024-06-21 14:01:42 +0000

    added suitable description

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./footer.css";
 import { FaTwitter } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import logo from "../../assets/logo__withwhite.png"
+import logo from "../../assets/logo__withwhite.png";
 
 function Footer() {
   return (
@@ -12,38 +13,51 @@ function Footer() {
       <footer>
         <div className="footer_sub_div">
           <div className="footer_logo">
-            <img src={logo} alt="Logo"/>
+            <img src={logo} alt="Logo" />
           </div>
 
           <div className="footer_icons">
-            <FaTwitter color="#ffffff" size={20} />
-            <FaFacebookF color="#ffffff" size={20} />
-            <FaInstagram color="#ffffff" size={20} />
-            <FaLinkedin color="#ffffff" size={20} />
+            <Link to="#">
+              <FaTwitter color="#ffffff" size={20} />
+            </Link>
+            <Link to="#">
+              <FaFacebookF color="#ffffff" size={20} />
+            </Link>
+            <Link to="#">
+              <FaInstagram color="#ffffff" size={20} />
+            </Link>
+            <Link to="#">
+              <FaLinkedin color="#ffffff" size={20} />
+            </Link>
           </div>
 
           <div className="footer_services">
-            <span id="footer_FAQ">FAQs</span>
-            <span id="footer_TOS">Terms of Service</span>
-            <span id="footer_PP">Privacy Policy</span>
+            <Link className="footer_links" to="#">
+              FAQs
+            </Link>
+            <Link className="footer_links" to="#">
+              Terms of Service
+            </Link>
+            <Link className="footer_links" to="#">
+              Privacy Policy
+            </Link>
           </div>
 
           <hr style={{ width: "1040px", color: "#ffffff" }} />
 
           <div>
             <span className="footer_desc">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              euismod libero id ante malesuada, ac efficitur lorem mollis.
-              Phasellus vitae erat ut turpis posuere finibus ac nec sapien. Duis
-              sed sapien nec ipsum feugiat lacinia. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Nullam euismod libero id ante
-              malesuada, ac efficitur lorem mollis. Phasellus vitae erat ut
-              turpis posuere finibus ac nec sapien. Duis sed sapien nec ipsum
+              Student Entrepreneurship HUB, a vibrant platform
+              where students can showcase and sell their innovative products and
+              services. Our mission is to empower young entrepreneurs by
+              providing them with the resources and community they need to
+              succeed. Connect, collaborate, and grow your business with us
+              today!
             </span>
           </div>
 
           <div className="footer_copyright">
-            &copy; {new Date().getFullYear()} Student Entrpreneurship HUB. All
+            &copy; {new Date().getFullYear()} Student Entrepreneurship HUB. All
             rights reserved.
           </div>
         </div>
