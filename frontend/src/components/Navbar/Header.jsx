11c4d1043a8a0a6112a51954fd683dab14b30b90
RonenTGreat <ronenh53@gmail.com>
2024-06-25 22:03:17 +0000

    changed header to make it more dynamic when user logs in

--- frontend/src/components/Navbar/Header.jsx
+++ frontend/src/components/Navbar/Header.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown, Button, Form } from "react-bootstrap";
+import { IoSearchOutline } from "react-icons/io5";
 import "./header.css";
 import logo from "../../assets/logo.png";
 import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../utils/AuthContext";
 
 function Header() {
+  const { authTokens, logout } = useAuth();
+
   return (
     <header className="py-3">
-      <Navbar expand="lg" py-3 className="navbar">
+      <Navbar expand="lg" className="navbar">
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>
@@ -19,28 +23,71 @@ function Header() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="navbar-nav mx-auto">
               <LinkContainer to="/AboutUs">
-                <Nav.Link href="#about-us" className="nav-text">
-                  About Us
-                </Nav.Link>
+                <Nav.Link className="nav-text">About Us</Nav.Link>
               </LinkContainer>
 
               <LinkContainer to="/faqs">
                 <Nav.Link className="nav-text">FAQs</Nav.Link>
               </LinkContainer>
 
-              <LinkContainer to="/help">
-                <Nav.Link className="nav-text">Help</Nav.Link>
-              </LinkContainer>
-
-              
+              {authTokens ? (
+                <>
+                  <NavDropdown
+                    title="Categories"
+                    id="basic-nav-dropdown"
+                    className="dropdown-link"
+                  >
+                    <NavDropdown.Item href="#action/3.1">
+                      Electronics
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Fashion
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Stationary
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <>
+                  <LinkContainer to="/help">
+                    <Nav.Link className="nav-text">Help</Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
             </Nav>
-            <Nav className="header-second ">
-              <LinkContainer to="/Login">
-                <Nav.Link className="nav-text">Login</Nav.Link>
-              </LinkContainer>
-              <Link to="/signup">
-                <Button className="btn start_selling_btn">Get Started</Button>
-              </Link>
+            <Nav className="header-second">
+              {authTokens ? (
+                <>
+                  <Form className="d-flex">
+                    <span className="search-container">
+                    <IoSearchOutline className="glass" />
+                    <Form.Control
+                      type="search"
+                      placeholder="Search..."
+                      className="search"
+                      aria-label="Search"
+                    /> 
+                    </span>
+                    
+                    <Button className="start_selling_btn">+ Start Selling </Button>
+                  </Form>
+                  <Nav.Link className="nav-text" onClick={logout}>
+                    Logout
+                  </Nav.Link>
+                </>
+              ) : (
+                <>
+                  <LinkContainer to="/login">
+                    <Nav.Link className="nav-text">Login</Nav.Link>
+                  </LinkContainer>
+                  <Link to="/signup">
+                    <Button className="btn start_btn">
+                      Get Started
+                    </Button>
+                  </Link>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
