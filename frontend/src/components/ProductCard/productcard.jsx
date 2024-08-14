46293ad0b44aa89c2bbda4cf16b73a83fa4cc5d2
RonenTGreat <ronenh53@gmail.com>
2024-08-14 10:29:36 +0000

    updated the ProductCard component to accept a productId prop and wrap the card with a Link

--- frontend/src/components/ProductCard/productcard.jsx
+++ frontend/src/components/ProductCard/productcard.jsx
@@ -1,17 +1,20 @@
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 import "../ProductCard/productcard.css";
 
-function ProductCard({ productImg, productCardTitle, productPrice }) {
+function ProductCard({ productId, productImg, productCardTitle, productPrice }) {
   return (
-    <Card className="product__card">
-      <Card.Img variant="top" src={productImg} />
-      <div className="product__card-description">
-        <p className="product__card-description-title">{productCardTitle}</p>
-        <p className="product__card-description-price">
-          Price: <span>GHS {productPrice}</span>
-        </p>
-      </div>
-    </Card>
+    <Link to={`/product/${productId}`} className="product__link">
+      <Card className="product__card">
+        <Card.Img variant="top" src={productImg} />
+        <div className="product__card-description">
+          <p className="product__card-description-title">{productCardTitle}</p>
+          <p className="product__card-description-price">
+            Price: <span>GHS {productPrice}</span>
+          </p>
+        </div>
+      </Card>
+    </Link>
   );
 }
 
