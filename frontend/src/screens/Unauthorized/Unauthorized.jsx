94c19fc001219865e50ffa9cca135e12e70cfa44
RonenTGreat <ronenh53@gmail.com>
2024-08-23 15:29:04 +0000

    styles unathorized page

--- frontend/src/screens/Unauthorized/Unauthorized.jsx
+++ frontend/src/screens/Unauthorized/Unauthorized.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import "../PageNotFound/PageNotFound.css";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import lock from "../../assets/lock-password.svg";
+import Header from "../../components/Navbar/Header";
+import Footer from "../../components/Footer/Footer";
 
-const Unauthorized = () => (
-  <div style={{ textAlign: "center", marginTop: "50px" }}>
-    <h1>403 - Unauthorized</h1>
-    <p>You do not have permission to view this page.</p>
-    <Link to="/">Go to Home</Link>
-  </div>
-);
+const PageNotFound = () => {
+  return (
+    <>
+      <Header />
+      <section className="notfound">
+        <img src={lock} alt="" />
+        <div>
+          <h1>401 error - Unauthorized</h1>
+          <p>You do not have permission to view this page.</p>
+          <Link to="/">
+            <Button className="signup__btn">Back to Homepage</Button>
+          </Link>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
 
-export default Unauthorized;
+export default PageNotFound;
